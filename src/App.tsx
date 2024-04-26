b457e39eb8ac2bde4665d3a8e929689635da77b2
Basit Balogun <basitbalogun10@gmail.com>
2024-04-26 22:20:36 +0100

    feat: add toastify and replace submit button with checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import Home from './pages/Home'
 import Analysis from './pages/Analysis'
 
@@ -8,24 +9,26 @@ import 'aos/dist/aos.css'
 
 function App() {
   return (
-    <Router>
-      <div className="h-screen bg-gray-100 px-7 pt-4 font-rubik text-[0.9rem] leading-[1.35rem] text-gray-900 dark:bg-gray-900 dark:text-gray-200">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/analysis" element={<Analysis />} />
-          <Route
-            path="*"
-            element={
-              <div className="flex h-screen items-center justify-center bg-gray-200">
-                <p className="text-center font-rubik text-2xl font-bold">
-                  404 PAGE NOT FOUND
-                </p>
-              </div>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="h-screen bg-gray-100 px-7 pt-4 font-rubik text-[0.9rem] leading-[1.35rem] text-gray-900 dark:bg-gray-900 dark:text-gray-200">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/analysis" element={<Analysis />} />
+            <Route
+              path="*"
+              element={
+                <div className="flex h-screen items-center justify-center bg-gray-200">
+                  <p className="text-center font-rubik text-2xl font-bold">
+                    404 PAGE NOT FOUND
+                  </p>
+                </div>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,14 @@ import { capitalizeString } from 'utils'
 
 const HomePage = () => {
   const [sideMenuIsVisible, setSideMenuIsVisible] = useState(true)
+  const [rememberData, setRememberData] = useState(false)
   const [fitnessLevelDropdownOpen, setFitnessLevelDropdownOpen] =
     useState(false)
   const [formData, setFormData] = useState({
-    allergies: [],
+    allergies: '',
     dietGoal: '',
     dietaryPreference: '',
-    healthConditions: [],
+    healthConditions: '',
     fitnessLevel: '',
     lifeStage: ''
   })
@@ -20,9 +21,19 @@ const HomePage = () => {
   useEffect(() => {
     const storedFormData = localStorage.getItem('formData')
     if (storedFormData) {
+      console.log('Data: ', JSON.parse(storedFormData))
       setFormData(JSON.parse(storedFormData))
     }
   }, [])
+
+  const handleRememberDataChange = () => {
+    setRememberData(!rememberData)
+
+    // Using the fact that setState actions are asynchronous (so remeberData won't be updated yet, hence using !rememberData)
+    if (!rememberData) {
+      localStorage.setItem('formData', JSON.stringify(formData))
+    }
+  }
 
   const handleFitnessLevelChange = (level) => {
     setFormData({ ...formData, fitnessLevel: level })
@@ -51,12 +62,6 @@ const HomePage = () => {
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    localStorage.setItem('formData', JSON.stringify(formData))
-    // You can perform any additional actions with the form data here
-  }
-
   return (
     <div className="relative overflow-hidden">
       <div
@@ -65,18 +70,19 @@ const HomePage = () => {
         } flex-col items-center justify-center`}
       >
         {/* A. Topmost section */}
-        <div className="mb-12 flex items-center justify-between px-4 py-2">
+        <div className="mb-12 flex items-center justify-between px-4 ">
           {/* Logo */}
           <div className="flex-1 text-center">
             <h1 className="font-[cursive] text-4xl font-medium tracking-wider">
-              healthALT
+              <span>health</span>
+              <span className="text-teal-500">ALT</span>
             </h1>
           </div>
           {!sideMenuIsVisible && (
             <div className="flex items-center space-x-4">
               <button
                 onClick={() => setSideMenuIsVisible(true)}
-                className="rounded-full p-2 transition-colors hover:bg-gray-800 dark:hover:bg-gray-600"
+                className="rounded-full p-2 transition-colors ease-in-out hover:bg-gray-800 dark:bg-teal-700 dark:hover:bg-teal-600"
               >
                 <IoIosOptions size={24} />
               </button>
@@ -99,10 +105,10 @@ const HomePage = () => {
             <input
               type="text"
               placeholder="What can I help you with"
-              className="w-full rounded-2xl border border-gray-300 py-4 pl-6 pr-24 outline-none focus:outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-800"
+              className="w-full rounded-2xl border border-gray-300 py-4 pl-6 pr-24 outline-none transition-colors ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-800 dark:hover:border-teal-500"
             />
             <div className="absolute right-4 top-1/2 flex -translate-y-1/2 space-x-2">
-              <button className="rounded-lg bg-red-600 px-3 py-2 dark:bg-gray-900">
+              <button className="rounded-lg px-3 py-2 dark:bg-gray-900">
                 <BsCamera size={18} />
               </button>
               <button className="flex items-center rounded-lg bg-teal-700 px-3 py-2 dark:bg-teal-700">
@@ -119,7 +125,7 @@ const HomePage = () => {
           sideMenuIsVisible ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
-        <div className="mt-4 flex justify-end">
+        <div className="mt-2 flex justify-end">
           <button
             onClick={() => setSideMenuIsVisible(false)}
             className="rounded-full p-2 transition-colors dark:hover:bg-gray-800"
@@ -131,7 +137,7 @@ const HomePage = () => {
           {/* <h2 className="mb-4 text-center text-2xl font-medium">
             Health Information
           </h2> */}
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className="mb-4">
               <label htmlFor="allergies" className="mb-2 block">
                 Allergies
@@ -141,7 +147,7 @@ const HomePage = () => {
                 name="allergies"
                 value={formData.allergies}
                 placeholder="Ex. Peanuts, Groundnut oil (comma-separated)"
-                className="w-full rounded-lg border px-4 py-2 outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900"
+                className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500"
                 onChange={handleFormChange}
               />
             </div>
@@ -152,10 +158,10 @@ const HomePage = () => {
               </label>
               <input
                 type="text"
-                name="allergies"
+                name="healthConditions"
                 value={formData.healthConditions}
-                placeholder="Ex. Diabetes, Heart Disease, Anaemia (comma-separated)"
-                className="w-full rounded-lg border px-4 py-2 outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900"
+                placeholder="Ex. Diabetes, Heart Disease (comma-separated)"
+                className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500"
                 onChange={handleFormChange}
               />
             </div>
@@ -166,10 +172,10 @@ const HomePage = () => {
               </label>
               <input
                 type="text"
-                name="allergies"
+                name="dietaryPreference"
                 value={formData.dietaryPreference}
                 placeholder="Ex. Halal, Vegetarian, Vegan (comma-separated)"
-                className="w-full rounded-lg border px-4 py-2 outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900"
+                className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500"
                 onChange={handleFormChange}
               />
             </div>
@@ -180,7 +186,7 @@ const HomePage = () => {
               </label>
               <div className="relative">
                 <button
-                  className={`w-full rounded-md border px-4 py-2 text-left dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 ${
+                  className={`w-full rounded-md border px-4 py-2 text-left transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500 ${
                     !formData.fitnessLevel ? 'text-gray-300/80' : ''
                   }`}
                   onClick={() => {
@@ -233,32 +239,62 @@ const HomePage = () => {
                 name="dietGoal"
                 value={formData.dietGoal}
                 placeholder="Enter your diet goal"
-                className="w-full rounded-lg border px-4 py-2 outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900"
+                className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500"
                 onChange={handleFormChange}
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="dietaryPreference" className="mb-2 block">
-                Dietary Preference
+              <label htmlFor="lifeStage" className="mb-2 block">
+                Life Stage
               </label>
               <input
                 type="text"
-                name="dietaryPreference"
-                value={formData.dietaryPreference}
-                placeholder="Enter your dietary preference"
-                className="w-full rounded-lg border px-4 py-2 outline-none focus:ring-2 dark:hover:border-teal-500 transition-colors ease-in-out focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900"
+                name="lifeStage"
+                value={formData.lifeStage}
+                placeholder="Ex. Pregnancy, Elderly, Menstrual Period, Nursing"
+                className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-teal-700 dark:bg-gray-900 dark:hover:border-teal-500"
                 onChange={handleFormChange}
               />
             </div>
 
-            {/* Add similar input fields for healthConditions and lifeStage */}
-            <div className="mt-6 flex justify-center">
-              <button
-                type="submit"
-                className="flex items-center rounded-md border border-teal-700 bg-teal-700 px-4 py-2 text-white "
-              >
-                Save Information <IoSendSharp size={18} className="ml-3" />
-              </button>
+            <div className="m space-y-1">
+              <p className="text-center text-xs text-gray-300/80">
+                Your data is safe and only stored on your device
+              </p>
+              <div className="flex items-center justify-center">
+                <input
+                  type="checkbox"
+                  id="rememberData"
+                  name="rememberData"
+                  checked={rememberData}
+                  onChange={handleRememberDataChange}
+                  className="sr-only mr-2" // Hide the default checkbox
+                />
+                <label
+                  htmlFor="rememberData"
+                  className={`relative mr-2 size-4 cursor-pointer rounded-[0.18rem] border border-teal-700 ${
+                    rememberData ? 'bg-teal-700' : 'bg-transparent'
+                  }`}
+                >
+                  {rememberData && (
+                    <svg
+                      className="absolute left-1/2 top-1/2 w-4 -translate-x-1/2 -translate-y-1/2 text-white"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M9 16.2l-4.6-4.6 1.4-1.4 3.2 3.2 7.2-7.2 1.4 1.4-8.6 8.6z"
+                      />
+                    </svg>
+                  )}
+                </label>
+                <button
+                  onClick={handleRememberDataChange}
+                  className="text-teal-500"
+                >
+                  Remember my data
+                </button>
+              </div>
             </div>
           </form>
         </div>
