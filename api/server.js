2cefc494270d533db53c9457e24c9c311f794a39
Basit Balogun <basitbalogun10@gmail.com>
2024-04-27 08:29:50 +0100

    fix: fix __dirname definition

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import apiRouter from "./router.js";
 
 const initializeExpressServer = async () => {
@@ -14,18 +14,13 @@ const initializeExpressServer = async () => {
     app.use("/api/v1/ai-conversation", apiRouter);
 
     if (process.env.NODE_ENV === "production") {
-        const __dirname = path.resolve(path.dirname(""));
-        app.use(express.static(path.join(__dirname, "../frontend/build")));
+        const __dirname = path.resolve();
+
+        app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
         app.get("*", (req, res) =>
             res.sendFile(
-                path.resolve(
-                    __dirname,
-                    "../",
-                    "frontend",
-                    "build",
-                    "index.html"
-                )
+                path.resolve(__dirname, "../", "frontend", "dist", "index.html")
             )
         );
     } else {
